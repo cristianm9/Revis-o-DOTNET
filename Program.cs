a364f7fa93eb2712d9304bacb221cb2960363ac3
Cristian Matheus Galindo de Brito <cristian96matheus@gmail.com>
2020-12-29 22:33:52 -0300

    Criação do Menu e README.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,43 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1- Inserir novo aluno.");
+            Console.WriteLine("2- Listar alunos.");
+            Console.WriteLine("3- Calcular média geral.");
+            Console.WriteLine("X- Sair.");
+            Console.WriteLine();
 
+            string opcaousuario = Console.ReadLine();
+
+            while (opcaousuario.ToUpper() != "X")
+            {
+                switch (opcaousuario)
+                {
+
+                    case "1":
+                        //Add aluno
+                        break;
+
+                    case "2":
+                        //List alunos
+                        break;
+
+                    case "3":
+                        //Media Geral
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                Console.WriteLine("Informe a opção desejada: ");
+                Console.WriteLine("1- Inserir novo aluno.");
+                Console.WriteLine("2- Listar alunos.");
+                Console.WriteLine("3- Calcular média geral.");
+                Console.WriteLine("X- Sair.");
+                Console.WriteLine();
+                opcaousuario = Console.ReadLine();
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,16 @@
+## Revisão sobre DOTNET C# :computer:
+
+### Assuntos abordados:
+
+- Estrutura de Algoritmos.
+
+- Variáveis. 
+- Instruções.
+- Objetos.
+- Structs , Interfaces e Enums.
+
+#### Projeto Simples de cadastro de Alunos:white_check_mark:
+
+- Inserir aluno novo.
+- Listar os alunos.
+- Calcular média geral.
